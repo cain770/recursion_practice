d45bf1ca667b78156e190687bb62a12dfd9e5eb8
Cain <143950811+cain770@users.noreply.github.com>
2023-11-16 11:49:48 +0000

    fixed countdown

--- recursion_practice.py
+++ recursion_practice.py
@@ -12,9 +12,13 @@ def pos_dec_to_binary(decimal,bit_list):
     
 #why does this not work? Fix it!
 def countdown(number):
-    print(number)
-    time.sleep(1)
-    countdown(number-1)
+    if number == 0:
+        return
+    else:
+        print(number)
+        time.sleep(1)
+        countdown(number-1)
+
 
 
 #try to complete this
